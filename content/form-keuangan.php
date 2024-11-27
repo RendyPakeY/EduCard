ca218d60c06fe1a2de21b6df16fd3b98f5b98242
unknown <rendykhaerulrizal@gmail.com>
2024-11-27 14:57:28 +0700

    updateBesar

---
+++ content/form-keuangan.php
@@ -0,0 +1,28 @@
+<div class="form-keuangan">
+                <div class="form-keuangan-value">
+                    <div class="left box">
+                        <img src="assets/img/uang-kiri.PNG" alt="foto">
+                        <h1>Tambah Saldo Kartu</h1>
+                        <div class="value">
+                            <p>Jumlah Penambahan</p>
+                            <div class="jumlah">
+                                <p>Rp</p>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="konfirmasi"><button class="blue-btn">konfirmasi</button></div>
+                    </div>
+                    <div class="right box">
+                        <img src="assets/img/uang-kanan.PNG" alt="foto">
+                        <h1>Pengembalian Saldo Kartu</h1>
+                        <div class="value">
+                            <p>Jumlah Pengembalian</p>
+                            <div class="jumlah">
+                                <p>Rp</p>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="konfirmasi"><button class="blue-btn">konfirmasi</button></div>
+                    </div>
+                </div>
+            </div>
---
+++ content/kontrol.php
@@ -0,0 +1,33 @@
+<div class="kontrol">
+                <div class="kontrol-value">
+                    <div class="table">
+                        <h1>Kontrol Kelas</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Alat</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Nama Produk</td>
+                                    <td><div class="check"><div class="off on"></div></div></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Nama Produk</td>
+                                    <td><div class="check"><div class="off "></div></div></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Nama Produk</td>
+                                    <td><div class="check"><div class="off "></div></div></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
