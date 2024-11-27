f5290d80dbb812f3018eca034ede453ff03c5958
unknown <rendykhaerulrizal@gmail.com>
2024-11-27 07:55:39 +0700

    fiturTambah1

---
+++ content/form-pembelian.php
@@ -0,0 +1,54 @@
+            <div class="form-pembelian">
+                <div class="form-pembelian-value">
+                    <div class="table">
+                        <h1>Form Pembelian Kantin</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Produk Pembelian</th>
+                                    <th>Harga satuan</th>
+                                    <th>Jumlah Produk</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Nama Produk</td>
+                                    <td>Harga</td>
+                                    <td>Status</td>
+                                    <td>Rp 2.000</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Nama Produk</td>
+                                    <td>Harga</td>
+                                    <td>Status</td>
+                                    <td>Rp 2.000</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Nama Produk</td>
+                                    <td>Harga</td>
+                                    <td>Status</td>
+                                    <td>Rp 2.000</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Total</td>
+                                    <td>Rp 2.000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <button class="blue-btn">Tambah Pembelian</button>
+                    <div class="submit">
+                        <div class="submit-value">
+                            <button class="blue-btn">Konfirmasi</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ content/form-peminjaman.php
@@ -0,0 +1,43 @@
+<div class="form-peminjaman">
+                <div class="form-peminjaman-value">
+                    <div class="table">
+                        <h1>Form Peminjaman Buku</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Buku</th>
+                                    <th>Pencipta</th>
+                                    <th>Jumlah</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Nama Produk</td>
+                                    <td>Harga</td>
+                                    <td>Status</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Nama Produk</td>
+                                    <td>Harga</td>
+                                    <td>Status</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Nama Produk</td>
+                                    <td>Harga</td>
+                                    <td>Status</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <button class="blue-btn">Tambah Pinjaman</button>
+                    <div class="submit">
+                        <div class="submit-value">
+                            <button class="blue-btn">Konfirmasi</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
