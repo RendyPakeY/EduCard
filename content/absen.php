5e7016156fc9b3b5a07564d9a6d0c0c1824e6d7d
unknown <rendykhaerulrizal@gmail.com>
2024-11-27 21:01:43 +0700

    lastUpdate

---
+++ content/absen.php
@@ -0,0 +1,38 @@
+<div class="absen">
+                <div class="absen-value">
+                    <div class="header box three">
+                        <div class="header-value one">
+                            <div class="left">
+                                <h1>Presentase<br>kehadiran Bulan ini</h1>
+                            </div>
+                            <div class="right">
+                                <h1>95%</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="main two box">
+                        <div class="left">
+                            <h1>Riwayat Kehadiran Semester</h1>
+                            <div class="riwayat">
+                                <!-- Mainkan database disini -->
+                                <!-- yang satu ini sebagai hard code -->
+                                <ul class="barang">
+                                    <li>Januari</li>
+                                    <li>Februari</li>
+                                    <li>Maret</li>
+                                    <li>April</li>
+                                </ul>
+                                <ul class="harga">
+                                    <li>100%</li>
+                                    <li>100%</li>
+                                    <li>100%</li>
+                                    <li>100%</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="right">
+                            <div id="chart"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ content/saldo.php
@@ -0,0 +1,39 @@
+<div class="saldo">
+                <div class="saldo-value">
+                    <div class="header box three">
+                        <div class="header-value one">
+                            <div class="left">
+                                <p>Jumlah saldo,</p>
+                                <h1>Rendy Khaerul Rizal</h1>
+                            </div>
+                            <div class="right">
+                                <h1>Rp 50.000</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="main two box">
+                        <div class="left">
+                            <h1>Kehadiran Semester</h1>
+                            <div class="riwayat">
+                                <!-- Mainkan database disini -->
+                                <!-- yang satu ini sebagai hard code -->
+                                <ul class="barang">
+                                    <li>Anda Membeli Onigiri</li>
+                                    <li>Anda Membeli Onigiri</li>
+                                    <li>Anda Membeli Onigiri</li>
+                                    <li>Anda Membeli Onigiri</li>
+                                </ul>
+                                <ul class="harga">
+                                    <li>Rp 8.000</li>
+                                    <li>Rp 8.000</li>
+                                    <li>Rp 8.000</li>
+                                    <li>Rp 8.000</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="right">
+                            <div id="chart"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
